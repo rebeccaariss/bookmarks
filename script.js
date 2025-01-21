109ee232496eb619fde6dc4fde26ef9a9322c813
Rebecca Ariss <63316843+rebeccaariss@users.noreply.github.com>
2025-01-21 00:26:28 -0500

    Add check for missing form values; add return value for validate function if checks pass

--- script.js
+++ script.js
@@ -21,13 +21,19 @@ window.addEventListener('click', (e) => (e.target === modal ? modal.classList.re
 function validate(nameValue, urlValue) {
   const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
   const regex = new RegExp(expression);
-  if (urlValue.match(regex)) {
-    alert('Match ✅');
+
+  if (!nameValue || !urlValue) {
+    alert('Please submit values for both fields.')
+    return false;
   }
+
   if (!urlValue.match(regex)) {
     alert('Please provide a valid URL.');
     return false;
   }
+
+  // if valid:
+  return true;
 };
 
 // Handle data from form:
